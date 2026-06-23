e1b6535d0bb9c03b26c5e2da0e3c724c37eac00b
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2026-06-23 10:07:53 +0200

    nou path dbt

--- dags/daily_dbt.py
+++ dags/daily_dbt.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 with DAG("DAG_daily_dbt", start_date=datetime(2021, 1, 1), schedule_interval="30 2 * * *", catchup=False) as dag:
     dbt_run = BashOperator(
         task_id='dbt_run',
-        bash_command='dbt run --project-dir=/home/airflow/dbt/daily ',
+        bash_command='/home/airflow/projdbt/.venv/bin/dbt run --project-dir=/home/airflow/dbt/daily ',
         email_on_failure=True,
         execution_timeout=timedelta(seconds=3600),
         email=Variable.get("mail_zulip"),
@@ -17,7 +17,7 @@ with DAG("DAG_daily_dbt", start_date=datetime(2021, 1, 1), schedule_interval="30
 
     dbt_test = BashOperator(
         task_id='dbt_test',
-        bash_command='dbt test --project-dir=/home/airflow/dbt/daily',
+        bash_command='/home/airflow/projdbt/.venv/bin/dbt test --project-dir=/home/airflow/dbt/daily',
         email_on_failure=True,
         email=Variable.get("mail_zulip"),
         dag=dag
